c77aa2a047cccd38c774ca90cda003b4d50607c6
Justin <jiheon.ham61@gmail.com>
2023-04-17 18:37:09 -0700

    added keys.js

---
+++ public/constants/keys.js
@@ -0,0 +1,11 @@
+config = {
+    DB_HOST: '54.71.40.98',
+    DB_NAME: 'f1',
+    DB_PASSWORD: 'keyboardPass1.',
+    DB_USER: 'server',
+    SESSION_KEY: 'thisisthesecretkey',
+    email: 'racegambit@gmail.com',
+    email_pass: 'rujohftxrocjqlbd'
+}
+
+exports.config = config;
